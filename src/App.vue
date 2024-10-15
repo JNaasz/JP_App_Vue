27f6962ebbaa20d252a92d04c3343eaba538e28e
Jennifer Naasz <naasz.jl@gmail.com>
2024-10-15 13:07:03 -0400

    chore: refactor file structure, move content of componentContainer into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,51 @@
 <template>
   <div id="app">
-    <ComponentContainer sheet-data="" />
+    <div class="main">
+    <div class="section-container">
+      <LandingPage msg="Well hello there" v-if="activeTab === 'Home'" />
+      <SheetComponent v-else :sheetData="sheetData" />
+    </div>
+    <div class="tab-container">
+      <TabComponent v-model="activeTab"></TabComponent>
+    </div>
+  </div>
   </div>
 </template>
 
 <script lang="ts">
-import ComponentContainer from './components/ComponentContainer.vue'
+import LandingPage from './layouts/LandingPage.vue';
+import SheetComponent from './layouts/SheetComponent.vue';
+import TabComponent from './components/TabComponent.vue';
+
+import { getSheetData } from './api/sheets';
 
 export default {
   name: 'App',
   components: {
-    ComponentContainer,
+    LandingPage,
+    SheetComponent,
+    TabComponent,
+  },
+  data() {
+    return {
+      sheetData: undefined as SheetData | undefined,
+      section: 'landing',
+      activeTab: 'Home',
+    };
+  },
+  watch: {
+    activeTab(newVal, oldVal) {
+      console.log('Tab changed from', oldVal, 'to', newVal);
+    }
+  },
+  async mounted() {
+    try {
+      const data = await getSheetData('dog,plants');
+      this.sheetData = data;
+      console.log('sheet data:', this.sheetData, typeof this.sheetData);
+    } catch (error) {
+      console.error('Error fetching sheet data:', error);
+    }
   }
 }
 </script>
